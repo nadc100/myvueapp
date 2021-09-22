1d12e0094ee666dfea6ae07b0620b2ad0f01fd87
nadc100 <nadc100@gmail.com>
2021-09-22 13:09:52 -0500

    populating the repo

---
+++ Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.html">biuti</a>
+            </li>
+            <li>
+                <a href="http://">citas</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+</script>
